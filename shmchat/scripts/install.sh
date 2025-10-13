3622221237933c53f85385a8e892cebdcf871769
derEchteJan <baumann-jan@gmx.net>
2025-10-13 18:48:13 +0200

    shmchat: added piper bedrock_server service file

--- shmchat/scripts/install.sh
+++ shmchat/scripts/install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 sudo cp ./build_linux/shmchat /usr/bin &&\
 sudo chmod 777 /usr/bin/shmchat &&\
-echo "installed shmchat to /usr/bin"
+echo "installed shmchat to /usr/bin" &&\
+
+sudo cp ./build_linux/piper /usr/bin &&\
+sudo chmod 777 /usr/bin/piper &&\
+echo "installed piper to /usr/bin"
